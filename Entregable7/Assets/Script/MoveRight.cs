fad42e613a6114fbb5f4d52286a5bee1b28d798d
Flora de la Cruz <91455093+Gingasi@users.noreply.github.com>
2022-02-14 10:30:15 +0100

    FIN

--- Entregable7/Assets/Script/MoveRight.cs
+++ Entregable7/Assets/Script/MoveRight.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class MoveRight : MonoBehaviour
 {
     public float speed = 6f;
-    private float Xlimit = 19f;
+    private float Xlimit = 22f;
     
 
-    
-    
+
+
+
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime); // Movimiento Continuo de derecha a izquierda
 
-        if (transform.position.x >= Xlimit)
+        if (transform.position.x >= Xlimit || transform.position.x <= -Xlimit)
         {
             Destroy(gameObject);
         }
+        
     }
 }
--- Entregable7/Assets/Script/SpawnManager.cs
+++ Entregable7/Assets/Script/SpawnManager.cs
@@ -8,7 +8,8 @@ public class SpawnManager : MonoBehaviour
     // A continuación los datos que limitan y establecen los lugares de instanciación de laos prefabs que instanciaremos según una lista 
     public GameObject[] FlipFlapprefabs;
     private Vector3 spawnPosition;
-    private float xRange = -19f;
+    private float xRight = 19f;
+    private float xLeft = -19f;
     private float yUp = 14f;
     private float yDown = 5;
     private float startTime = 2f;
@@ -16,6 +17,8 @@ public class SpawnManager : MonoBehaviour
     private float randomX;
     private float randomY;
     private int randomIndex;
+    private Quaternion Rotte180 = Quaternion.Euler(0, 180, 0);
+
 
     private PlayerController PlayerControllerScript;
 
@@ -26,20 +29,45 @@ public class SpawnManager : MonoBehaviour
         PlayerControllerScript = FindObjectOfType<PlayerController>();
     }
 
-   
+    
 
     public Vector3 RandomSpawnPosition()
     {
+
+       int RandomSpawn = Random.Range(0, 2);
         randomY = Random.Range(yDown, yUp);
-        return new Vector3(xRange, randomY, 0);
+        randomX = Random.Range(xLeft, xRight);
+
+        if (RandomSpawn == 0)
+        {
+            return new Vector3(xLeft, randomY, 0);
+        }
+        else 
+        {
+            return new Vector3(xRight, randomY, 0);
+        }
+
     }
 
     public void SpawnFlipFlap()
     {
         randomIndex = Random.Range(0, FlipFlapprefabs.Length);
         spawnPosition = RandomSpawnPosition();
-        Instantiate(FlipFlapprefabs[randomIndex], spawnPosition,
+        
+
+
+
+        if (spawnPosition.x <= 0)  
+        {
+            Instantiate(FlipFlapprefabs[randomIndex], spawnPosition,
             FlipFlapprefabs[randomIndex].transform.rotation);
+        }
+        else
+        {
+            Instantiate(FlipFlapprefabs[randomIndex], spawnPosition,
+            Rotte180);
+        }
+
 
         if (PlayerControllerScript.gameOver)
         {
